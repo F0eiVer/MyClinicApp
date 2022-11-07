fcb90c4254ac2ba239251a40bdef5b22d6ca3e7e
F0eiVer <arefev030303@mail.ru>
2022-11-07 19:56:52 +1000

    Add DAL and connection with database

---
+++ MyClinicApp.DAL/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyClinicApp.Domain.Classes;
+
+namespace MyClinicApp.DAL
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
--- MyClinicApp/Startup.cs
+++ MyClinicApp/Startup.cs
@@ -8,6 +8,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MyClinicApp.DAL;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore;
 
 namespace MyClinicApp
 {
@@ -24,6 +27,10 @@ namespace MyClinicApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            var connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDBContext>(options =>
+                options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
