f28e2ac9552a0abb491e68831112324836c926ec
F0eiVer <arefev030303@mail.ru>
2023-01-11 17:05:52 +1000

    change language version

---
+++ MyClinicApp.DAL/Classes/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyClinicApp.Domain.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClinicApp.DAL.Classes
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Appointment>? Appointments { get; set; }
+
+        public DbSet<Doctor>? Doctors { get; set; }
+
+        public DbSet<Role>? Roles { get; set; }
+
+        public DbSet<Specialization>? Specializations { get; set; }
+
+        public DbSet<TimeTable>? TimeTables { get; set; }
+
+        public DbSet<User>? Users { get; set; }
+    }
+}
