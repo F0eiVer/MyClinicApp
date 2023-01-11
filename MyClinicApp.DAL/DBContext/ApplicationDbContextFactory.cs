7f1cdc9cb771926c88d5c6683c9c4b43b49c7efc
F0eiVer <arefev030303@mail.ru>
2023-01-12 00:06:54 +1000

    Changed structure. Added ApplicationDbContextFactory.

---
+++ MyClinicApp.DAL/DBContext/ApplicationDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using MyClinicApp.DAL.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClinicApp.DAL.DBContext
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+
+        // Я не стал заморачиваться и скрывать пароль, тк этот проект создается в учебном плане.
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseNpgsql(
+            $"Server=localhost;Port=5432;Database=Scrubs;Username=postgres;Password=Crjhgbjy123"
+            );
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
